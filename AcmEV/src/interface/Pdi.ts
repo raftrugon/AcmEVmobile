a44131088cc39a41d55ecbeeee07d0eb9b1cd27f
Rafael Trujillo <raftrugon@alum.us.es>
2018-05-12 12:43:23 +0200

    Modelo de datos PDI

---
+++ AcmEV/src/interface/Pdi.ts
@@ -0,0 +1,13 @@
+export interface Pdi {
+
+  id:number;
+  name:string;
+  surname:string;
+  nif:string;
+  address:string;
+  phone:string;
+  email:string;
+  department:number[];
+  subjects:number[];
+
+}
